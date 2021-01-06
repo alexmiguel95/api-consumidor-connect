cd04033bb39873005ed8491e3b8d358e2a973b9c
DENIS RAFAEL KORB <denisrafaelk@hotmail.com>
2021-01-06 20:32:16 -0300

    feature: sqlalchemy configuration

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from app.views.status import bp_home
+from models import db
+from environs import Env
+
+
+def create_app():
+    env = Env()
+    env.read_env()
+
+
+    app = Flask(__name__)
+
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = env.bool('SQLALCHEMY_TRACK_MODIFICATIONS')
+    app.config['SQLALCHEMY_DATABASE_URI'] = env.str('SQLALCHEMY_DATABASE_URI')
+    app.register_blueprint(bp_home)
+    db.init_app(app)
+
+    return app
