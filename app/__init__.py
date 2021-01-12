5e9d053e352bdb213f6bf877edc8e1041f985c21
Alex Miguel <alexmiguel95@gmail.com>
2021-01-12 20:24:13 -0300

    Instalado FLASK-RESTFul

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from app.views.status import bp_home
 from app.models import db
 from environs import Env
 
@@ -13,7 +12,7 @@ def create_app():
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = env.bool('SQLALCHEMY_TRACK_MODIFICATIONS')
     app.config['SQLALCHEMY_DATABASE_URI'] = env.str('SQLALCHEMY_DATABASE_URI')
-    app.register_blueprint(bp_home)
+
     db.init_app(app)
 
     return app
