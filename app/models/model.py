afd6307b33a370e62f13ccf7d973a0dd4362ff87
Alex Miguel <alexmiguel95@gmail.com>
2021-01-13 09:48:24 -0300

    Create schemas

--- app/models/model.py
+++ app/models/model.py
@@ -1,16 +1,19 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_marshmallow import Marshmallow
+from marshmallow import fields
 
 
 db = SQLAlchemy()
 mg = Migrate()
+ma = Marshmallow()
 
 
 # Tabela auxilar N:N entre Produtores e Consumidores
-produtores_consumidores = db.Table(
-    "produtores_consumidores",
-    db.Column("produtores_id", db.Integer, db.ForeignKey("produtores.id")),
-    db.Column("consumidores_id", db.Integer, db.ForeignKey("consumidores.id"))
+tb_produtores_consumidores = db.Table(
+    "tb_produtores_consumidores",
+    db.Column("fk_produtores", db.Integer, db.ForeignKey("produtores.id")),
+    db.Column("fk_consumidores", db.Integer, db.ForeignKey("consumidores.id"))
 )
 
 
@@ -26,6 +29,13 @@ class Produtores(db.Model):
         back_populates="produtos_produtores"
     )
 
+    # Relação N:N  entre Produtores e Consumidores
+    produtores_consumidores = db.relationship(
+        "Consumidores",
+        secondary=tb_produtores_consumidores,
+        back_populates='consumidores_produtores'
+    )
+
 
 class Produtos(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -34,7 +44,7 @@ class Produtos(db.Model):
     link_foto = db.Column(db.String)
     link_video = db.Column(db.String)
 
-    # Relação N:1 Entre Produtos e Produtos e Produtores
+    # Relação N:1 Entre Produtos e Produtores
     fk_produtores = db.Column(db.Integer, db.ForeignKey("produtores.id"))
     produtos_produtores = db.relationship(
         "Produtores",
@@ -48,12 +58,45 @@ class Consumidores(db.Model):
     email = db.Column(db.String)
     telefone = db.Column(db.String)
 
+    # Relação N:N  entre Consumidores e Produtores
+    consumidores_produtores = db.relationship(
+        "Produtores",
+        secondary=tb_produtores_consumidores,
+        back_populates="produtores_consumidores"
+    )
 
-""" Marshmallow """
-# ProdutoresSchema
+
+# Schemas
+class ProdutoresSchema(ma.SQLAlchemySchema):
+
+    class Meta:
+        model = Produtores
+
+    id = ma.auto_field()
+    nome = ma.auto_field()
+    email = ma.auto_field()
+    telefone = ma.auto_field()
 
 
-# ConsumidoresSchema
+class ConsumidoresSchema(ma.SQLAlchemySchema):
+
+    class Meta:
+        model = Consumidores
+
+    id = ma.auto_field()
+    nome = ma.auto_field()
+    email = ma.auto_field()
+    telefone = ma.auto_field()
+    consumidores_produtores = fields.Nested(ProdutoresSchema, many=True)
 
 
-# ProdutosSchema
+class ProdutosSchema(ma.SQLAlchemySchema):
+
+    class Meta:
+        model = Produtos
+
+    id = ma.auto_field()
+    nome = ma.auto_field()
+    descricao = ma.auto_field()
+    link_foto = ma.auto_field()
+    link_video = ma.auto_field()
